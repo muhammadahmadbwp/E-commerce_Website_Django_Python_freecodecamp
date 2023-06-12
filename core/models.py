76cb77983171f27eec2defdf687ff026f8cdab05
muhammad ahmad <muhammad.ahmad@dha-tasdeeq.com>
2023-06-12 18:45:35 +0500

    Handling payments with stripe

--- core/models.py
+++ core/models.py
@@ -76,6 +76,8 @@ class Order(models.Model):
     ordered = models.BooleanField(default=False)
     billing_address = models.ForeignKey(
         'BillingAddress', on_delete=models.SET_NULL, blank=True, null=True)
+    payment = models.ForeignKey(
+        'Payment', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
@@ -97,3 +99,13 @@ class BillingAddress(models.Model):
 
     def __str__(self):
         self.user.username
+
+class Payment(models.Model):
+
+    stripe_charge_id = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
